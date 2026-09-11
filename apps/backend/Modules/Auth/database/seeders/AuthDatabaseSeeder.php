4dc4816ea31dd4c95a76fb5fb150e1d96d51bbf3
Armin Babak <iamarminbabak@outlook.com>
2026-09-11 21:22:51 +0330

    fix(backend/auth): assign super-admin role to only the first seeded user

--- apps/backend/Modules/Auth/database/seeders/AuthDatabaseSeeder.php
+++ apps/backend/Modules/Auth/database/seeders/AuthDatabaseSeeder.php
@@ -3,9 +3,7 @@
 namespace Modules\Auth\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Modules\Auth\Models\Role;
 use Modules\Auth\Models\User;
-use Modules\Auth\Schemas\GuardsEnum;
 use Modules\Auth\Schemas\User\UserSchema;
 
 class AuthDatabaseSeeder extends Seeder
@@ -25,18 +23,13 @@ class AuthDatabaseSeeder extends Seeder
             UserSchema::COMMENT => null,
         ]);
 
-        $admin = User::query()->create([
+        User::query()->create([
             UserSchema::PASSWORD => '12345678',
             UserSchema::EMAIL => 'admin@baxela.com',
             UserSchema::EMAIL_VERIFIED_AT => now(),
             UserSchema::IS_ACTIVE => true,
             UserSchema::COMMENT => null,
         ]);
-
-        $admin->assignRole(Role::query()->firstOrCreate([
-            'name' => Role::SUPER_ADMIN,
-            'guard_name' => GuardsEnum::WEB->value,
-        ]));
 
         $this->call(AccessDatabaseSeeder::class);
     }
